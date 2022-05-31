d711810f4a4d203ca91a6604ed0f03df906a9dfe
David Vergaray <david.vergaray.a@gmail.com>
2022-05-31 14:53:57 -0400

    Fix port problem

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.1.1'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 # Ruby linters
 gem 'rubocop', '>= 1.0', '< 2.0'
 
--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,13 @@ Bundler.require(*Rails.groups)
 
 module HelloRailsBackEnd
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
